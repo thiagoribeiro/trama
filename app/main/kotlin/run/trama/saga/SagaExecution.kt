a2c17c15d78cb07e4c6a2d88adef9744bca43bf0
Thiago Azeredo <thiagoribeiro@gmail.com>
2026-04-08 17:14:52 -0300

    fix serializer

--- app/main/kotlin/run/trama/saga/SagaExecution.kt
+++ app/main/kotlin/run/trama/saga/SagaExecution.kt
@@ -12,6 +12,7 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonEncoder
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonPrimitive
 
@@ -296,7 +297,11 @@ object JsonElementFlexSerializer : KSerializer<JsonElement> {
         PrimitiveSerialDescriptor("JsonElementFlex", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: JsonElement) {
-        encoder.encodeString(Json.encodeToString(JsonElement.serializer(), value))
+        if (encoder is JsonEncoder) {
+            encoder.encodeJsonElement(value)
+        } else {
+            encoder.encodeString(Json.encodeToString(JsonElement.serializer(), value))
+        }
     }
 
     override fun deserialize(decoder: Decoder): JsonElement {
